352f5ffe53921b2477e91a012d5c5e1252020aa4
Acizza <jgit@tuta.io>
2020-09-04 14:20:12 -0700

    frontend: Apply selected theme via onchange / onblur event instead of onclick

--- frontend/src/components/MainPanel/Settings/Theme.svelte
+++ frontend/src/components/MainPanel/Settings/Theme.svelte
@@ -12,14 +12,13 @@
 </script>
 
 <Input name="theme" label="Site Theme">
-  <select name="theme" bind:value>
+  <select
+    name="theme"
+    bind:value
+    on:change={applySelected}
+    on:blur={applySelected}>
     {#each Object.entries(Theme) as [display, value]}
-      <option
-        {value}
-        selected={value === initialTheme}
-        on:click={applySelected}>
-        {display}
-      </option>
+      <option {value} selected={value === initialTheme}>{display}</option>
     {/each}
   </select>
 </Input>
